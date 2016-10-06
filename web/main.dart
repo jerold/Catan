2cf81735e2f9f5272f4536aca7f21caf0c8940f6
dominicfrost <frostationdevice@gmail.com>
2016-10-06 12:05:17 -0600

    pinned some deps and removed js injection.

--- web/main.dart
+++ web/main.dart
@@ -5,7 +5,6 @@ import 'package:react/react_dom.dart' as react_dom;
 
 import 'package:catan/catan.dart';
 
-
 main() async {
   GameModule module = new GameModule();
   await module.load();
@@ -14,4 +13,6 @@ main() async {
 
   react_dom.render(module.components.content(), querySelector(SELECTOR_CONTENT));
   react_dom.render(module.components.dimmer(), querySelector(SELECTOR_DIMMER));
+
+  window.onContextMenu.listen((e) => e.preventDefault());
 }
