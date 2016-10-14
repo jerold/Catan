4e7252de5ce3ed3501b52a8bd24a1bc5b2d60db1
Ben Echols <lologarithm@gmail.com>
2016-10-13 22:22:36 -0600

    Update gitignore and fixed install script

--- golib/buildclient.sh
+++ golib/buildclient.sh
@@ -4,7 +4,7 @@ set -e
 
 pushd $GOPATH/src/github.com/jerold/Catan/golib
 netgen --input=catannet/messages.ng --gendart=true
-mv catannet/catannet.dart ../lib/src/catannet
+mv catannet/catannet.dart ../lib/src/catannet/catannet.dart
 popd
 
 pushd $GOPATH/src/github.com/jerold/Catan/golib/client/bindings
--- initgo.sh
+++ initgo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./golib/init_tools.sh
+./golib/install_tools.sh
 ./golib/buildclient.sh
 ./golib/buildserver.sh
