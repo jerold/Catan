7a07af75027917093c64b868f771a0ef14abc4c2
Ben Echols <lologarithm@gmail.com>
2016-10-26 04:04:40 -0600

    fix init go

--- initgo.sh
+++ initgo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# Get go deps
+go get -u golang.org/x/net/websocket
+
 ./golib/install_tools.sh
 ./golib/buildclient.sh
 ./golib/buildserver.sh
-
-# Get go deps
-go get -u golang.org/x/net/websocket
